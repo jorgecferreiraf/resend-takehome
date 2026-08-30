8a5b26a379ef7f00a87417e59406550e6cec2ff8
jorgecferreiraf <jorge.cferreiraf@gmail.com>
2026-08-30 12:19:21 -0300

    Route update

--- app/api/send/route.tsx
+++ app/api/send/route.tsx
@@ -1,6 +1,6 @@
 import { EmailTemplate } from '../../../components/email-template';
 import { Resend } from 'resend';
-import * as React from 'react';
+import fs from 'fs';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -9,8 +9,19 @@ export async function POST() {
     const { data, error } = await resend.emails.send({
       from: 'Test <onboarding@resend.dev>',
       to: ['jorge.cferreiraf@gmail.com'],
-      subject: 'Failed payment',
-      react: <EmailTemplate firstName="Jorge" />,
+      subject: 'TakeHome Challenge — Billing Failure Email',
+      react: (
+        <EmailTemplate
+          firstName="Jorge"
+          repoUrl="https://github.com/jorgecferreiraf/resend-takehome"
+        />
+      ),
+      attachments: [
+        {
+          filename: 'submission.txt',
+          content: fs.readFileSync('./submission.txt'),
+        },
+      ],
     });
 
     if (error) {
