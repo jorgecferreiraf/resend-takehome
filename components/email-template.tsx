a80eed32f0a4924e35c700edae05f4fc1d5ca62d
jorgecferreiraf <jorge.cferreiraf@gmail.com>
2026-08-30 12:20:03 -0300

    Template update

--- components/email-template.tsx
+++ components/email-template.tsx
@@ -2,16 +2,24 @@ import * as React from "react";
 
 interface EmailTemplateProps {
   firstName: string;
+  repoUrl: string;
 }
 
 export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
   firstName,
+  repoUrl,
 }) => (
   <div>
-      <p>Hi {firstName},</p>
-      <p>We were unable to process your latest payment.</p>
-      <p>Please review your payment method in the Billing Page to avoid service interruption.</p>
-    </div>
+    <p>Hi {firstName},</p>
+    <p>We were unable to process your latest payment.</p>
+    <p>Please review your payment method in the Billing Page to avoid service interruption.</p>
+    <p>Sincerely,</p>
+    <p>Billing Team</p>
+    <p>
+      You can find the full project here:{" "}
+      <a href={repoUrl}>{repoUrl}</a>
+    </p>
+  </div>
 );
 
 export default EmailTemplate;
